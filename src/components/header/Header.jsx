3f658e4133660739cb2c4d4c536e0df6aa1d1090
alexandra-perez <alexandraperezny@gmail.com>
2024-05-04 13:06:10 -0400

    build header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,16 @@
-export default function Header(){
-    return (<></>)
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header className="Header">
+      <div>MyNotes App</div>
+      <nav>
+        <ul>
+          <li>My Notes</li>
+          <li>Add New Note</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
