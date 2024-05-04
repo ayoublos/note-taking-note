cdb45c3361e5e46d9530f51704b4ce03e33a5de2
alexandra-perez <alexandraperezny@gmail.com>
2024-05-04 15:34:01 -0400

    add search functionality

--- src/components/notelist/NoteList.jsx
+++ src/components/notelist/NoteList.jsx
@@ -1,19 +1,45 @@
-import Note from "../note/Note"
-import "./NoteList.scss"
-export default function NoteList({data}){
+import './NoteList.scss';
+import Note from '../note/Note';
+import { useState } from 'react';
+export default function NoteList({ data }) {
+  const [notes, setNotes] = useState(data);
+  const [filteredNotes, setFilteredNotes] = useState(data);
+  const [searchTitle, setSearchTitle] = useState('');
 
-    return (
-        <>
-            <div className="search">
-                <form>
-                    <input type="text" name="search" id="search" placeholder="Search"/>
-                </form>
-            </div>
-        <div className="noteList">
-          {data.map((note) => (
-            <Note key={note.id} note={note} />
-          ))}
-        </div>
-      </>
-    );
+  function handleTextChange(event) {
+    const title = event.target.value;
+    const result = title.length
+      ? filterNotes(title, notes)
+      : notes;
+    setSearchTitle(title);
+    setFilteredNotes(result);
+  }
+
+
+  function filterNotes(search, notes) {
+    return notes.filter((note) => {
+      return note.title.toLowerCase().match(search.toLowerCase());
+    });
+  }
+
+  return (
+    <>
+      <div className="search">
+        <form>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search"
+            onChange={handleTextChange}
+          />
+        </form>
+      </div>
+      <div className="noteList">
+        {filteredNotes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </div>
+    </>
+  );
 }
