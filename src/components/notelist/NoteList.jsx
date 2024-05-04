8ab57a4dcbe42f7e146581bb1e92e078eeff8764
alexandra-perez <alexandraperezny@gmail.com>
2024-05-04 14:56:47 -0400

    add search bar

--- src/components/notelist/NoteList.jsx
+++ src/components/notelist/NoteList.jsx
@@ -3,8 +3,17 @@ import "./NoteList.scss"
 export default function NoteList({data}){
 
     return (
+        <>
+            <div className="search">
+                <form>
+                    <input type="text" name="search" id="search" placeholder="Search"/>
+                </form>
+            </div>
         <div className="noteList">
-            {data.map(note => <Note key={note.id} note={note}/>)}
+          {data.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
         </div>
-    )
+      </>
+    );
 }
