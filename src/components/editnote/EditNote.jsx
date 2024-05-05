cf493162596cc4421b61d507c9548697b7b6f118
ayoublos <abelibel@pursuit.org>
2024-05-05 16:24:58 -0700

    finish and move to css

--- src/components/editnote/EditNote.jsx
+++ src/components/editnote/EditNote.jsx
@@ -1,92 +1,100 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./EditNote.scss"
+import "./EditNote.scss";
 
-export default function EditNote () {
+export default function EditNote() {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate()
-    
-    const { id } = useParams()
+  const { id } = useParams();
 
-    const [allNotes, setAllNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState({
+    title: "",
+    body: "",
+    category: "",
+  });
 
-    useEffect(() => {
-      fetch('https://my-json-server.typicode.com/runquan-ray-zhou/demo/notes' + "/" + id)
-        .then((response) => response.json())
-        .then((response) => {
-          setAllNotes(response);
-        });
-    }, []);
+  useEffect(() => {
+    fetch("https://6637c889288fedf69381538c.mockapi.io/api/v1/notes" + "/" + id)
+      .then((response) => response.json())
+      .then((response) => {
+        setCurrentNote(response);
+      });
+  }, []);
 
-    // const currentNote = allNotes.filter(note => note.id === id)[0]
-    
-    // const [updateNote, setUpdateNote] = useState(currentNote)
+  function handleTextChange(e) {
+    setCurrentNote({ ...currentNote, [e.target.id]: e.target.value });
+  }
 
-    // function handleTextChange(e) {
-    //     setUpdateNote({...updateNote,[e.target.id]: e.target.value})
-    // }
-
-    // function updateSingleNote(e) {
-    //     e.preventDefault()
-    //     const index = notes.findIndex(note => note.id === updateNote.id)
-    //     const newNotes = [...notes]
-    //     newNotes.splice(index, 1, updateNote)
-    //     setFilteredNotes(newNotes)
-    //     setNotes(newNotes)
-    //     navigate("/")
-    // }
-
-    // function deleteNote() {
-    //     const index = allNotes.findIndex(note => note.id === updateNote.id)
-    //     const newNotes = [...notes]
-    //     newNotes.splice(index, 1)
-    //     setFilteredNotes(newNotes)
-    //     setNotes(newNotes)
-    //     navigate("/")
-    //   }
-        
-    return(
-        <div className="note">
-            <div className="note__container">
-                <form>
-                    <label htmlFor="title">
-                        <h1>
-                            <input
-                            // onChange={handleTextChange}
-                            className="note__title" 
-                            id="title" 
-                            type="text" 
-                            value={allNotes.title}/>
-                        </h1>
-                    </label>
-                    <label htmlFor="category">
-                        <h3>
-                            <input
-                            // onChange={handleTextChange}
-                            className="note__category" 
-                            id="category" 
-                            type="text" 
-                            value={allNotes.category}/>
-                            </h3>
-                    </label>
-                    <label htmlFor="content">
-                        <p>
-                            <textarea
-                            // onChange={handleTextChange} 
-                            className="note__body" 
-                            id="content" 
-                            style={{width: "100%"}}
-                            rows="25" 
-                            value={allNotes.body}>
-                            </textarea>
-                        </p>
-                    </label>
-                    <div className="update-delete__button">
-                        <button type="submit">Update This Note</button>
-                        <button> Delete This Note</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+  function updateSingleNote(e) {
+    e.preventDefault();
+    const options = {
+      method: "PUT",
+      body: JSON.stringify(currentNote),
+      headers: { "Content-Type": "application/json" },
+    };
+    return fetch(
+      `https://6637c889288fedf69381538c.mockapi.io/api/v1/notes/${id}`,
+      options
     )
+      .then((response) => {
+        return response.json();
+      })
+      .then(() => {
+        navigate("/");
+      });
+  }
+  function destroyNote(id) {
+    const options = { method: "DELETE" };
+    return fetch(
+      `https://6637c889288fedf69381538c.mockapi.io/api/v1/notes/${id}`,
+      options
+    ).then(() => navigate(`/`));
+  }
+
+  return (
+    <div className="note">
+      <div className="note__container">
+        <form onSubmit={updateSingleNote}>
+          <label htmlFor="title">
+            <h1>
+              <input
+                onChange={handleTextChange}
+                className="note__title"
+                id="title"
+                type="text"
+                value={currentNote.title}
+              />
+            </h1>
+          </label>
+          <label htmlFor="category">
+            <h3>
+              <input
+                onChange={handleTextChange}
+                className="note__category"
+                id="category"
+                type="text"
+                value={currentNote.category}
+              />
+            </h3>
+          </label>
+          <label htmlFor="content">
+            <p>
+              <textarea
+                onChange={handleTextChange}
+                className="note__body"
+                id="body"
+                style={{ width: "100%" }}
+                rows="25"
+                value={currentNote.body}
+              ></textarea>
+            </p>
+          </label>
+          <div className="update-delete__button">
+            <button type="submit">Update This Note</button>
+            <button onClick={() => destroyNote(id)}>Delete This Note</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
