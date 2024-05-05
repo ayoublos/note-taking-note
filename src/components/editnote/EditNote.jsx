bee921f4eb0a3d29bc0a7290ff59043c192763de
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-05-04 23:18:33 -0400

    add input fields and handle text change

--- src/components/editnote/EditNote.jsx
+++ src/components/editnote/EditNote.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./EditNote.scss"
 
 export default function EditNote ({ notes, setNotes, setFilteredNotes }) {
 
     let navigate = useNavigate()
-
+    
     const { id } = useParams()
 
     const currentNote = notes.filter(note => note.id === id)[0]
+    
+    const [updateNote, setUpdateNote] = useState(currentNote)
+
+    function handleTextChange(e) {
+        setUpdateNote({...updateNote,[e.target.id]: e.target.value})
+    }
+
+    console.log(currentNote)
 
     function deleteNote() {
-        const index = notes.findIndex(note => note.id === currentNote.id)
+        const index = notes.findIndex(note => note.id === updateNote.id)
         const newNotes = [...notes]
         newNotes.splice(index, 1)
         setFilteredNotes(newNotes)
@@ -22,12 +31,54 @@ export default function EditNote ({ notes, setNotes, setFilteredNotes }) {
     return(
         <div className="note">
             <div className="note__container">
-                <h2>{currentNote.title}</h2>
-                <h3>Entered On: {currentNote.date}</h3>
-                <h3>{currentNote.category}</h3>
-                <ul>{currentNote.tags.map((tag,i) => <li key={i}>{tag}</li>)}</ul>
-                <p>{currentNote.content}</p>
-                <div className="deleteButton"><button onClick={deleteNote}>Delete This Note</button></div>
+                <form>
+                    <label htmlFor="title">
+                        <h1>
+                            <input
+                            onChange={handleTextChange}
+                            className="note__title" 
+                            id="title" 
+                            type="text" 
+                            value={updateNote.title}/>
+                        </h1>
+                    </label>
+                    <label htmlFor="date">
+                        <h3>Entered On: 
+                            <input
+                            onChange={handleTextChange}
+                            className="note__date" 
+                            id="date" 
+                            type="text" 
+                            value={updateNote.date}/>
+                        </h3>
+                    </label>
+                    <label htmlFor="category">
+                        <h3>
+                            <input
+                            onChange={handleTextChange}
+                            className="note__category" 
+                            id="category" 
+                            type="text" 
+                            value={updateNote.category}/>
+                            </h3>
+                    </label>
+                    <ul>{currentNote.tags.map((tag,i) => <li key={i}>{tag}</li>)}</ul>
+                    <label htmlFor="body">
+                        <p>
+                            <textarea
+                            onChange={handleTextChange} 
+                            className="note__body" 
+                            id="body" 
+                            style={{width: "100%"}} 
+                            value={updateNote.content}>
+                            </textarea>
+                        </p>
+                    </label>
+                    <div className="update-delete__button">
+                        <button type="submit">Update This Note</button>
+                        <button onClick={deleteNote}>Delete This Note</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
