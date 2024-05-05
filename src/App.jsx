57a94c3c3eb21e81785723a757783ca9a1f61fa1
ayoublos <abelibel@pursuit.org>
2024-05-05 11:42:26 -0700

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Header from "./components/header/Header.jsx";
 import NoteList from "./components/notelist/NoteList.jsx";
 import Note from "./components/note/Note.jsx";
-import EditNote from "./components/editnote/EditNote.jsx";
 import AboutUs from "./components/about us/AboutUs.jsx";
 import PageNotFound from "./components/pagenotfound/PageNotFound.jsx";
 import NewNoteForm from "./components/newnoteform/NewNoteForm.jsx";
@@ -12,31 +11,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import data from './data/data.json'
 
 function App() {
-const [notes, setNotes] = useState(data);
-const [filteredNotes, setFilteredNotes] = useState(notes);
-const [searchTitle, setSearchTitle] = useState('');
+  console.log(data)
 
-function handleTextChange(event) {
-  const title = event.target.value;
-  const result = title.length ? filterNotes(title, notes) : notes;
-  setSearchTitle(title);
-  setFilteredNotes(result);
-}
-
-function filterNotes(search, notes) {
-  return notes.filter((note) => {
-    return note.title.toLowerCase().match(search.toLowerCase());
-  });
-}
-  
   return (
     <main>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<NoteList handleTextChange={handleTextChange} filteredNotes={filteredNotes} />} />
-          <Route path="/notes" element={<NoteList handleTextChange={handleTextChange} filteredNotes={filteredNotes} />} />
-          <Route path="/notes/:id/edit" element={<EditNote notes={notes} setNotes={setNotes} setFilteredNotes={setFilteredNotes}/>} />
+          <Route path="/" element={<NoteList data={data} />} />
+          <Route path="/notes" element={<NoteList data={data} />} />
+          <Route path="/notes/:id/edit" element={<Note />} />
           <Route path="/notes/new" element={<NewNoteForm />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/*" element={<PageNotFound />} />
--- src/components/newnoteform/NewNoteForm.jsx
+++ src/components/newnoteform/NewNoteForm.jsx
@@ -1,3 +1,87 @@
-export default function NewNoteForm(){
-    return (<></>)
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./NewNoteForm.scss";
+
+export default function NewNoteForm() {
+  let navigate = useNavigate();
+  const [newNote, setNewNote] = useState({
+    title: "",
+    body: "",
+    category: "",
+  });
+  function textChangeHandler(event) {
+    setNewNote({ ...newNote, [event.target.id]: event.target.value });
+  }
+  // function createNote(note) {
+  //   const options = {
+  //     method: "POST",
+  //     body: JSON.stringify(note),
+  //     headers: { "Content-Type": "application/json" },
+  //   };
+  //   return fetch(`${URL}/notes/`, options).then((response) => {
+  //     return response.json();
+  //   });
+  // }
+
+  // function submitHandle(e) {
+  //   e.preventDefault();
+  //   setNewNote({ title: "", body: ``, category: `` });
+  //   createNote(newNote)
+  // }
+ 
+  return (
+    <>
+      <form onSubmit={submitHandle}>
+        <label htmlFor="">
+          Title
+          <br />
+          <input
+            className="title"
+            id="title"
+            value={newNote.title}
+            onChange={textChangeHandler}
+            placeholder="Please input a title"
+            type="text"
+          />
+        </label>
+        <br />
+        <label htmlFor="">
+          Category
+          <br />
+          <input
+            className="category"
+            id="category"
+            value={newNote.category}
+            onChange={textChangeHandler}
+            placeholder="Please input a category"
+            type="text"
+          />
+        </label>
+        <br />
+
+        <label htmlFor="">
+          Content
+          <br />
+          <input
+            className="content"
+            id="body"
+            value={newNote.body}
+            onChange={textChangeHandler}
+            placeholder="Please input a content"
+            type="text"
+          />
+        </label>
+        <br />
+        <button
+          onClick={() => {
+            navigate(`/`);
+          }}
+          type="text"
+        >
+          Cancel
+        </button>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
