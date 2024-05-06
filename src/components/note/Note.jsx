8bb902b8dd9978bee5f84827ab05d24d1552be31
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-05-06 18:37:38 -0400

    fix blank note problem

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -3,14 +3,14 @@ import "./Note.scss"
 export default function Note({note}){
 
     return (
+    <Link to={`/notes/${note.id}/edit`}>
         <div className="singleNote">
-            <Link to={`/notes/${note.id}/edit`}>
             <div className="singNote__container">
                 <h2>{note.title}</h2>
                 <h3>{note.category}</h3>
                 <p>{note.body}</p>
             </div>
-            </Link>
         </div>
+    </Link>
     )
 }
